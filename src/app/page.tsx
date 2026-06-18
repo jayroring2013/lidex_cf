ea1bb4bd12e267f1f90f67595ff4531bd7101efe
jayroring2013 <jayroring2013@gmail.com>
2026-06-18 10:19:22 +0700

    perf: use cached homepage payload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import { createElement } from 'react'
 import HomeClient from '@/components/HomeClient'
-import { fetchHomeData } from '@/lib/db'
+import { getCachedHomeData } from '@/lib/cachedHomeData'
 
 export const revalidate = 3600
 
 export default async function Home() {
-  const homeData = await fetchHomeData()
+  const homeData = await getCachedHomeData()
   return createElement(HomeClient, { initialData: homeData })
 }
